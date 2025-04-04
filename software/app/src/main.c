08e3ccc66cf41595d50d0d0020a77bd4d3dd8fe6
Hugo Frisk <hugopaf@outlook.com>
2025-04-04 21:19:03 +0200

    cleanup and correct led name

--- software/app/src/main.c
+++ software/app/src/main.c
@@ -12,30 +12,30 @@
  #define SLEEP_TIME_MS   1000
  
  /* The devicetree node identifier for the "led0" alias. */
- #define LED0_NODE DT_ALIAS(led0)
+ #define LED_IO_NODE DT_NODELABEL(led_io)
  
  /*
   * A build error on this line means your board is unsupported.
   * See the sample documentation for information on how to fix this.
   */
- static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+ static const struct gpio_dt_spec led_io = GPIO_DT_SPEC_GET(LED_IO_NODE, gpios);
  
  int main(void)
  {
      int ret;
      bool led_state = true;
  
-     if (!gpio_is_ready_dt(&led)) {
+     if (!gpio_is_ready_dt(&led_io)) {
          return 0;
      }
  
-     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+     ret = gpio_pin_configure_dt(&led_io, GPIO_OUTPUT_ACTIVE);
      if (ret < 0) {
          return 0;
      }
  
      while (1) {
-         ret = gpio_pin_toggle_dt(&led);
+         ret = gpio_pin_toggle_dt(&led_io);
          if (ret < 0) {
              return 0;
          }
